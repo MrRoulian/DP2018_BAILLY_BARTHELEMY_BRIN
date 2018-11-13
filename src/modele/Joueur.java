b14a6681791ba2517aeb00a2f1719f848c1e6290
Julien <julien.bailly4@etu.univ-lorraine.fr>
2018-11-13 12:04:46 +0100

    modif

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -3,8 +3,15 @@ package modele;
 public abstract class Joueur {
 	
 	protected Joueur adversaire;
+	protected Grille grid;
+	
+	public Joueur(Grille grid){
+		this.grid=grid;
+	}
 
-	public abstract void setAdversaire(Joueur j);
+	public void setAdversaire(Joueur j){
+		this.adversaire = j;
+	}
 	public abstract void jouerTour();
 	
 }
