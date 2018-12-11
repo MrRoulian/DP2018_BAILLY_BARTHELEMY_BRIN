c7467d695506032703e42dba2d78057d3a66427b
Julien <julien.bailly4@etu.univ-lorraine.fr>
2018-12-11 17:18:55 +0100

    je sais plus

--- src/main/Serveur.java
+++ src/main/Serveur.java
@@ -9,13 +9,14 @@ import java.rmi.RemoteException;
 
 import modele.Humain;
 import modele.Partie;
+import modele.Robot;
 
 public class Serveur {
 
 	public static void main(String[] args) throws RemoteException, AlreadyBoundException, UnknownHostException, MalformedURLException {
 		System.out.println("Constructing server implementation...");
 		
-		Partie p = new Partie(new Humain(null,1),new Humain(null,2));
+		Partie p = new Partie(new Humain(null,1),new Robot(null,2));
 		
 		System.out.println("Binding server implementation to registry...");
 		
