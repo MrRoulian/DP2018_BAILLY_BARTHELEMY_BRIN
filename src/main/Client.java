f34160d2afbb09a71e2f5fa7e6252f6181883bce
Julien <julien.bailly4@etu.univ-lorraine.fr>
2018-12-13 14:05:08 +0100

    modif client

--- src/main/Client.java
+++ src/main/Client.java
@@ -15,34 +15,38 @@ import modele.Joueur;
 import vue.VueGraphique;
 
 public class Client {
-	
+
 	public static void main(String[] args) throws NamingException, RemoteException, NotBoundException, MalformedURLException, UnknownHostException
 	{
 		//Pour rejoindre un serveur, remplacer le InetAddress.getLocalHost().getHostAddress() par l'adresse ip du sereur
 		InterfacePartie p = null;
-		
+
 		if (args.length == 1){
 			p = (InterfacePartie) Naming.lookup("rmi://" + args[0] + "/Bataille_navale");
 		} else if (args.length == 0){
 			p = (InterfacePartie) Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/Bataille_navale");
+			System.out.println(	"Pour utiliser le client en ligne avec un autre joueur il faut mettre son adresse ip en paramètre\n"+
+					"java Client 192.168.43.17 par exemple");
 		} else {
 			System.out.println(	"Pour utiliser le client en ligne avec un autre joueur il faut mettre son adresse ip en paramètre\n"+
-								"java Client 192.168.43.17 par exemple");
+					"java Client 192.168.43.17 par exemple");
 		}
-		
+
 		Joueur moi = null;
 		Joueur adversaire = null;
 		boolean isJoueur1 = false;
-		
-		if (p.getJoueur1Libre()){
-			p.setJoueur1Libre(false);
-			isJoueur1=true;
-			p.setJoueur1(new Humain(null, 1));
-		} else {
-			p.setJoueur2(new Humain(null, 2));
-			p.setEveryPlayersReady(true);
+
+		if (!p.isEveryPlayersReady()){
+			if (p.getJoueur1Libre()){
+				p.setJoueur1Libre(false);
+				isJoueur1=true;
+				p.setJoueur1(new Humain(null, 1));
+			} else {
+				p.setJoueur2(new Humain(null, 2));
+				p.setEveryPlayersReady(true);
+			}
 		}
-		
+
 		while(!p.isEveryPlayersReady()){
 			try {
 				Thread.sleep(500);
@@ -52,7 +56,7 @@ public class Client {
 			}
 			System.out.println("En attente d'adversaire");
 		}
-		
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -68,19 +72,19 @@ public class Client {
 			moi = p.getJoueur2();
 			adversaire = p.getJoueur1();
 		}
-		
+
 		VueGraphique vg = new VueGraphique(moi,adversaire,p);
-		
+
 		Joueur moiCourrant = null;
 		Joueur adversaireCourrant = null;
-		
+
 		while(true){
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-			
+
 			if (moi.equals(p.getJoueur1())){
 				moiCourrant = p.getJoueur1();
 				adversaireCourrant = p.getJoueur2();
@@ -93,16 +97,16 @@ public class Client {
 			vg.updateJoueur(moiCourrant, adversaireCourrant);
 			moiCourrant.setNumBateauSelectionne(moi.getNumBateauSelectionne());
 			moiCourrant.setCiblePourTirer(moi.getCiblePourTirer());
-			
+
 			//Si j'ai pu jouer mon tour
 			if (p.jouerTour(moiCourrant)){				
 				moi.setCiblePourTirer(null);	
 			}
-			
+
 			//Mets à jour l'affichage
 			vg.update();
 		}
-		
+
 	}
-	
+
 }
