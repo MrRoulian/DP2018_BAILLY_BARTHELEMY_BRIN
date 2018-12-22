a0826d5bf203fac16afeb62c61a4e62a292b6746
MrRoulian <julien.bailly4@etu.univ-lorraine.fr>
2018-12-22 23:20:47 +0100

    edit du .bat

--- Server.bat
+++ Server.bat
@@ -1,2 +1,2 @@
-start rmiregistry -J-Djava.rmi.server.codebase=http://localhost/modele/modele/InterfacePartie
+start rmiregistry -J-Djava.rmi.server.codebase=http://localhost/modele/InterfacePartie
 java main.Serveur 1 8 4
